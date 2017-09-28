4b27dd0539bb96c720fa2389dd728af41c8f58b6
Tim Freeman <d.timothy.freeman@gmail.com>
2017-09-28 13:08:24 -0400

    added navbar placeholder component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import NewUserForm from './components/NewUserForm'
 import LogInForm from './components/LogInForm'
 import NewTripForm from './components/NewTripForm'
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <NavBar />
        <NewUserForm />
        <LogInForm />
        <NewTripForm />
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div style={{height:50, backgroundColor: 'black', color: 'white'}}>
+      Log In Link
+      Log Out Link
+    </div>
+  )
+}
+
+export default NavBar;
